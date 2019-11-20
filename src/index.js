a1bf62c0bd1bdf217f6e2f38e06ccd6b6d36e11a
Katrina_Dierking <kalynndier@gmail.com>
2019-11-20 17:15:30 -0500

    initial state moved to reducer.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from "react-redux";
+
+
 import { createStore } from "redux";
+import { Provider } from "react-redux";  //bridge between react & redux
+import { reducer } from './reducers';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
 
 const store = createStore(reducer);
+console.log(store);
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-    <Provider store={store}>
+    <Provider store={store}> 
     <App />,
     </Provider>,
     rootElement);
