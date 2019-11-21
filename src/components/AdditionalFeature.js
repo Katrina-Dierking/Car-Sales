dac92e613b70c367d7551bfbae8ae4aa263ef245
Katrina_Dierking <kalynndier@gmail.com>
2019-11-20 23:25:15 -0500

    still troubleshooting. Having issues with (buyItem)

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,11 @@
 import React from 'react';
+// import { buyItem } from '../App';
 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button onClick ={() => props.buyItem(props.feature)} className="button">Add This Item</button>
+      <button onClick = {() => props.buyItem (props.feature, props.car)} className="button">Add This Item</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
